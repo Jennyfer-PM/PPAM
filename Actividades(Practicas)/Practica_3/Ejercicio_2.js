6ae2c4e9a597761fd25c6c8dc2e8f9a7adcfcbf7
Jennyfer-PM <124049932@upq.edu.mx>
2025-09-25 23:09:46 -0600

    Ejercicio unopractica 3 y avance ejercicio 2

---
+++ Actividades(Practicas)/Practica_3/Ejercicio_2.js
@@ -0,0 +1,9 @@
+function verificarUsuario(usuario){
+    return new promise ((resolve, rejected) => 
+    );
+}
+
+promise.then(res => console.log(res))
+promise.catch(err => console.error(err));
+
+verificarUsuario("admin")
---
+++ Actividades(Practicas)/Practica_3/main.js
@@ -0,0 +1,5 @@
+import {restar} from './utils.js';
+
+console.log("La resta de 8-5 es: " + restar(8, 5));
+console.log("La resta de 7-3 es: " + restar(7, 3));
+console.log("La resta de 9-1 es: " + restar(9, 1));
