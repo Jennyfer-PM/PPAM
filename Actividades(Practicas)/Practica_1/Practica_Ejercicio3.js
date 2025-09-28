1cd9b963f84dfe1eaed1303a632c487e517af1e1
Jennyfer-PM <124049932@upq.edu.mx>
2025-09-27 18:13:04 -0600

    Reporte de la practica 1

--- Actividades(Practicas)/Practica_1/Practica_Ejercicio3.js
+++ Actividades(Practicas)/Practica_1/Practica_Ejercicio3.js
@@ -1,4 +1,5 @@
-const saludoPersonalizado = (a, b) => console.log("Hola, me llamo " + a + " y tengo " + b + " años.");
+const saludoPersonalizado = (a, b) => 
+    console.log("Hola, me llamo " + a + " y tengo " + b + " años.");
 
 saludoPersonalizado("Dulce", 20);
 saludoPersonalizado("Jessie", 19);
