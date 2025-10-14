9f53e3631a7d620cdf0f33e96e48b1181934b433
Jennyfer-PM <124049932@upq.edu.mx>
2025-10-13 18:58:18 -0600

    Agregado de Menu
    Se agrego una interfaz de menu, para, a partir de ahi ingresar a las practicas

--- Intro/App.js
+++ Intro/App.js
@@ -1,15 +1,14 @@
 
 //1. Imports: Zona de importaciones
 import ContadorScreen from './screens/ContadorScreen';
-
+import MenuScreen from './screens/MenuScreen';
 
 //2. Main: Zona de componentes
 export default function App() {
 
   return (
-
-    <ContadorScreen> </ContadorScreen>
-
+    <MenuScreen></MenuScreen>
+  
   );
 }
 
---
+++ Intro/screens/MenuScreen.js
@@ -0,0 +1,50 @@
+import { Text, StyleSheet, View, Button } from 'react-native'
+import React, { useState } from 'react'
+import BotonesScreen from './BotonesScreen';
+import ContadorScreen from './ContadorScreen';
+
+export default function MenuScreen() {
+  const [screen, setScreen] = useState('menu');
+
+  switch(screen){
+    case 'contador': 
+        return <ContadorScreen/>;
+    case 'botones': 
+        return <BotonesScreen/>;
+    case 'menu':
+        default:
+        return (
+            <View style={styles.container}>
+                <Text style={styles.menu}> Menu de Practicas </Text>
+                <View style={styles.botoncitos}>
+                <Button color='green' onPress={()=>setScreen('contador')} title='Practica Contador'></Button>
+                <Button color='purple' onPress={()=>setScreen('botones')} title='Practica Botones'></Button>
+                </View>
+            </View>
+    )
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#46ebc7ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+
+    menu: {
+    color:'rgba(224, 31, 31, 1)',
+    fontSize:40,
+    fontFamily: 'Times New Roman',
+    fontWeight: 'bold',
+  },
+
+  botoncitos:{
+    marginTop:20,
+    flexDirection:'row',
+    gap:20,
+  },
+})
