3ddd23fb50026c436336d9d20f61b6844107ee1a
Jennyfer-PM <124049932@upq.edu.mx>
2025-11-06 20:31:52 -0600

    Commit limite

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,39 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, ImageBackground, Image, Dimensions} from 'react-native';
-import { SectionList } from 'react-native-web';
+import { StyleSheet, Text, View, ScrollView, ImageBackground, Image, Dimensions, Alert} from 'react-native';
+import { Button, SectionList } from 'react-native-web';
 
 const imagen = require("../Examen2doP/assets/Restaurante.jpeg");
 
 export default function App() {
+  const mostrarAlerta = () => {
+    window.alert('¿Deseas ordenar este platillo?')
+  }
+
   const categorias = [
-    {titulo: 'Entrada', data: ['Sopa', 'Ensalada', 'Tostadas']},
-    {titulo: 'Platos principales', data: ['Pavo', 'Cordero', 'Cangrejo']},
-    {titulo: 'Bebidas', data: ['Agua', 'Vino', 'Whisky']},
-    {titulo: 'Postres', data: ['Pastel', 'Pay', 'Gelatina']}
+    {titulo: 'Entrada', data: ['Sopa - Sopa simple - 40$', 'Ensalada - Ensalada de lechuga - 70$']},
+    {titulo: 'Platos principales', data: ['Pavo - Pieza de pavo recien hecha - 630$', 'Langosta - Langosta preparada al gusto - 500$']},
+    {titulo: 'Bebidas', data: ['Agua - Agua natural con o sin hielo - 20$', 'Vino - Vino a elección del consumidor - 65$']},
+    {titulo: 'Postres', data: ['Pastel - Pastel de diversos sabores - 50$', 'Pay - Pay de diversos sabores - 45$']}
   ]
 
   return (
     <ImageBackground
     source={imagen}
-    resizeMode='cover'>
+    resizeMode='cover'
+    style={styles.background}>
     <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={true}>
-      <Text> --BIENVENIDO AL MENÚ DEL RESTAURANTE-- </Text>
-      <Text> A continuación encontraras el menú digital del restaurante, podras ver los pedidos y realizar tu orden. </Text>
+      <ScrollView 
+      style={styles.scrollArea}
+      showsVerticalScrollIndicator={true}>
+      <Text style={styles.title}> --BIENVENIDO AL MENÚ DEL RESTAURANTE-- </Text>
+      <Text style={styles.subtitulo}> A continuación encontraras el menú digital del restaurante, podras ver los pedidos y realizar tu orden. </Text>
 
-      <View>
-        <Text>MENÚ</Text>
+      <View style={styles.listContainer}>
+        <Text style={styles.title}>MENÚ</Text>
         <SectionList
           sections={categorias}
           renderItem={({item})=>(
-            <Text>{item}</Text>
+            <Text style={styles.item}>{item}</Text>
+          )}
+
+          renderSectionFooter={({})=>(
+            <Button color='brown' title='Ordenar' onPress={mostrarAlerta}/>
           )}
           
           renderSectionHeader={({section})=>(
             <Text style={styles.titulo}>{section.titulo}</Text>
-          )}/>
+          )}
+          />
       </View>
       </ScrollView>
+      <StatusBar style="auto" />
     </View>
     </ImageBackground>
   );
@@ -51,9 +64,42 @@ const styles = StyleSheet.create({
   titulo: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  subtitulo: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
   },
   background: {
     width: width,
-    height
+    height: height,
+  },
+  scrollArea: {
+    flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    marginBottom: 20,
+  },
+  item: {
+    padding: 10,
+    marginHorizontal: 5,
+    marginVertical: 10,
+    borderRadius: 5,
+    backgroundColor: '#be5656ff',
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '400',
+  },
+  boton: {
+
   }
 });
